8713821dccd7ff1d76d99f68d3d26bd83459a581
Max Lateman <twoliquid@gmail.com>
2019-02-12 00:58:22 +0300

    Add some markups for filter search query builder

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -31,14 +31,31 @@ class UserRepository
         $user = $user->newQuery();
 
         if (isset($params['age'])) {
-            if (isset($params['age']['from'])) {
-                $birthDate = Carbon::now()->addYears(-$params['age']['from']);
-                $user->where('data->date_of_birth', '<=', $birthDate->toDateString());
-            }
 
-            if (isset($params['age']['to'])) {
-                $birthDate = Carbon::now()->addYears(-$params['age']['to']);
-                $user->where('data->date_of_birth', '>=', $birthDate->toDateString());
+            if (isset($params['age']['from']) && isset($params['age']['to'])) {
+                if ($params['age']['from'] == $params['age']['to']) {
+                    $startFrom = Carbon::now()->addYears(-$params['age']['to'] - 1)->toDateString();
+                    $endTo = Carbon::now()->addYears(-$params['age']['to'])->toDateString();
+
+                    $user->where('data->date_of_birth', '>=', $startFrom)
+                        ->where('data->date_of_birth', '<=', $endTo);
+                } else {
+                    $birthDateFrom = Carbon::now()->addYears(-$params['age']['from']);
+                    $birthDateTo = Carbon::now()->addYears(-$params['age']['to']);
+
+                    $user->where('data->date_of_birth', '<=', $birthDateFrom->format('Y-m-d'))
+                        ->where('data->date_of_birth', '>=', $birthDateTo->format('Y-m-d'));
+                }
+            } else {
+                if (isset($params['age']['from'])) {
+                    $birthDate = Carbon::now()->addYears(-$params['age']['from']);
+                    $user->where('data->date_of_birth', '<=', $birthDate->format('Y-m-d'));
+                }
+
+                if (isset($params['age']['to'])) {
+                    $birthDate = Carbon::now()->addYears(-$params['age']['to']);
+                    $user->where('data->date_of_birth', '>=', $birthDate->format('Y-m-d'));
+                }
             }
         }
 
@@ -47,7 +64,11 @@ class UserRepository
         }
 
         if (isset($params['hobby'])) {
-            $user->whereJsonContains('data->hobby', $params['hobby']);
+            $user->where(function ($query) use ($params) {
+                foreach ($params['hobby'] as $hobby) {
+                    $query->orWhereJsonContains('data->hobby', $hobby);
+                }
+            });
         }
 
         return $user->get();
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->json('data')->nullable();
+            $table->jsonb('data')->nullable();
             $table->timestamps();
         });
     }
